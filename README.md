d0c7a4731b5df1a855a970c37f0f0919a0966731
Christoph Höller <christoph.hoeller@googlemail.com>
2018-12-21 09:47:38 +0100

    business card v1

--- README.md
+++ README.md
@@ -1,9 +1,8 @@
-It's me, Tierney!
 
 # Usage
 Via npx:
 ```
-npx bitandbang
+npx choeller
 ```
 
 Not including how to use it globally because I'm not sure why you'd want this as a global command. That'd be creepy.
--- bin/card.js
+++ bin/card.js
@@ -14,33 +14,49 @@ const options = {
 
 // Text + chalk definitions
 const data = {
-  name: chalk.white('Tierney Cyren /'),
-  handle: chalk.cyan('bitandbang'),
-  work: chalk.white('Senior Cloud Developer Advocate at Microsoft'),
-  twitter: chalk.cyan('https://twitter.com/bitandbang'),
-  github: chalk.cyan('https://github.com/bnb'),
-  linkedin: chalk.cyan('https://linkedin.com/in/bitandbang'),
-  web: chalk.cyan('https://bnb.im'),
-  npx: chalk.white('npx bitandbang'),
+  name: chalk.white('Christoph Höller /'),
+  handle: chalk.cyan('choeller'),
+  work: chalk.white('CEO of beyond interfaces GmbH & Co. KG, Trainer & Software Consultant'),
+  twitter: chalk.cyan('https://twitter.com/chris_hoeller'),
+  github: chalk.cyan('https://github.com/choeller'),
+  xing: chalk.cyan('https://www.xing.com/profile/Christoph_Hoeller'),
+  web: chalk.cyan('http://christoph-hoeller.net/'),
+  location: chalk.cyan('Cologne, Germany'),
+  contact: chalk.cyan('mail@christoph-hoeller.net'),
+  stack: chalk.cyan('Angular, Vue.js, TypeScript, Node.js, Java, Spring, Docker, Kubernetes'),
+  npx: chalk.white('npx choeller'),
   labelWork: chalk.white.bold('      Work:'),
   labelTwitter: chalk.white.bold('   Twitter:'),
   labelGitHub: chalk.white.bold('    GitHub:'),
-  labelLinkedIn: chalk.white.bold('  LinkedIn:'),
+  labelXING: chalk.white.bold('      XING:'),
+  labelStack: chalk.white.bold('Tech-Stack:'),
   labelWeb: chalk.white.bold('       Web:'),
+  labelContact: chalk.white.bold('   Contact:'),
+  labelLocation: chalk.white.bold('  Location:'),
   labelCard: chalk.white.bold('      Card:')
 }
 
 // Actual strings we're going to output
 const newline = '\n'
-const heading = `${data.name} ${data.handle}`
+const heading = `${data.name} ${data.contact}`
 const working = `${data.labelWork}  ${data.work}`
 const twittering = `${data.labelTwitter}  ${data.twitter}`
 const githubing = `${data.labelGitHub}  ${data.github}`
-const linkedining = `${data.labelLinkedIn}  ${data.linkedin}`
+const xing = `${data.labelXING}  ${data.xing}`
+const techStack = `${data.labelStack}  ${data.stack}`
 const webing = `${data.labelWeb}  ${data.web}`
+const contact = `${data.labelContact}  ${data.contact}`
+const location = `${data.labelLocation}  ${data.location}`
 const carding = `${data.labelCard}  ${data.npx}`
 
 // Put all our output together into a single variable so we can use boxen effectively
-const output = heading + newline + newline + working + newline + twittering + newline + githubing + newline + linkedining + newline + webing + newline + newline + carding
+const output = heading + newline + newline + 
+working + newline + 
+techStack + newline + 
+twittering + newline + 
+githubing + newline + 
+xing + newline + 
+webing + newline + newline +
+carding
 
 console.log(chalk.green(boxen(output, options)))
